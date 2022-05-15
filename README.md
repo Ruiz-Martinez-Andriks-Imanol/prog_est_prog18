7dca632f0af625f2111ccd8cfe7d2dec04fe68bc
Ruiz-Martinez-Andriks-Imanol <105221206+Ruiz-Martinez-Andriks-Imanol@users.noreply.github.com>
2022-05-14 19:08:21 -0500

    prog_est_prog18
    Programa que imprime un mensaje usando funciones.

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # prog_est_prog18
+Autor: Andriks Imanol Ruiz Mártinez, Fecha: 28/02/2022 <br>
+Escuela: Universidad del Valle de Mexico Campus Villahermosa <br>
+Materia: Programación Estructrada <br>
+Ciclo: 01/2022</p>
+<br>
+<p>Este es un programa de ciclos y condicionales en Lenguaje C de la materia de Programación Estructurada<br>
+Muestra el uso de:
+  <ul>
+    <li>Variables enteras</li>
+    <li>printf para mostrar mensajes</li>
+    <li>scanf</li>
+    <li>Funciones</li>
+    <li>Prototipos</li>
+    <li>El uso de include para las librerias</li>
+    <li>for</li>
+    <li>Contador en for</li>    
+    <li>Comentarios para la documentación interna del programa</li>
+    </ul>
+    </p>
+<br>
+Objetivo: Hcer un programa que imprime un mensaje usando funciones.
+<br>
+<br>
+<p>Breve descripción:
+Es un programa que tiene una función que imprime un mensaje de saludo  encerrado en un marco de asteriscos
+para ejemplificar el uso de funciones void
+<br>
+</p>
---
+++ prog18.cpp
@@ -0,0 +1,38 @@
+/* Autor: Andriks Imanol Ruiz Mártinez, Realizado: 28/02/2022
+Escuela: Universidad del Valle de Mexico Campus Villahermosa
+Materia: Programación Estructrada
+Ciclo: 01/2022
+
+Este es un programa de ciclos y condicionales en Lenguaje C de la materia de Programación Estructurada
+Muestra el uso de:
+
+    -Variables enteras
+    -printf para mostrar mensajes
+    -scanf
+    -Funciones
+    -Prototipos
+    -El uso de include para las librerias
+    -for
+    -Contador en for 
+    -Comentarios para la documentación interna del programa
+*/
+#include<stdio.h>
+//Prototipo de funciones
+void saludar();
+//main
+int main(){
+	printf("Inicio el main...\n");
+	saludar();
+	printf("Termino el main.\n");
+	saludar();
+	for (int i=0;i<10;i++){
+		saludar();
+	}
+	return 0;
+}
+//Funcion que imprime un saludo en un marco de asteriscos
+void saludar(){
+	printf("***********************************\n");
+	printf("*	 H O L A	M U N D O !!!	*\n");
+	printf("***********************************\n");
+}
